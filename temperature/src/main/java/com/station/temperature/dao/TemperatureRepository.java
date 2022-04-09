d3a04c594f093c24562e0886378c1c553291b542
klchib3 <klchib3@gmail.com>
2022-04-09 10:21:37 +0800

    push

--- temperature/src/main/java/com/station/temperature/dao/TemperatureRepository.java
+++ temperature/src/main/java/com/station/temperature/dao/TemperatureRepository.java
@@ -11,14 +11,14 @@ import com.station.temperature.entity.Temperature;
 @Repository
 public interface TemperatureRepository extends JpaRepository<Temperature, Long> {
 	
-	String sql = "select station station,avg(value) avg from temperature where datastamp >= :startTime and datastamp < :endTime " 
-			+  "GROUP BY station, UNIX_TIMESTAMP(datastamp) DIV (15*60)";
+//	String sql = "select station station,avg(value) avg from temperature where datastamp >= :startTime and datastamp < :endTime " 
+//			+  "GROUP BY station, UNIX_TIMESTAMP(datastamp) DIV (15*60)";
 	
 	
 	@Query(nativeQuery = true, value = "select * from temperature where datastamp >= :startTime and datastamp < :endTime order by datastamp ")
 	List<Temperature> getBetweenDatastamp(@Param("startTime") String startTime,@Param("endTime") String endTime);
 	
-	@Query(nativeQuery = true, value = sql)
-	List<Object> getBetweenDatastamp1(@Param("startTime") String startTime,@Param("endTime") String endTime);
+//	@Query(nativeQuery = true, value = sql)
+//	List<Object> getBetweenDatastamp1(@Param("startTime") String startTime,@Param("endTime") String endTime);
 
 }
