dfc500c4228033054d1806f6a7757afb3349b54a
klchib3 <klchib3@gmail.com>
2022-04-11 23:19:34 +0800

    push

--- temperature/src/test/java/com/station/temperature/AbstractTest.java
+++ temperature/src/test/java/com/station/temperature/AbstractTest.java
@@ -4,6 +4,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.station.temperature.dao.TemperatureRepository;
 import com.station.temperature.service.InsertDBSerice;
@@ -15,6 +16,9 @@ public class AbstractTest {
 	
 	@Autowired
 	protected InsertDBSerice insertDBSericeImpl;
+	
+	@Autowired
+	protected WebApplicationContext webApplicationContext;
 	
 	@Autowired
 	protected TemperatureRepository temperatureRepository;
--- temperature/src/test/java/com/station/temperature/TemperatureUnitTest.java
+++ temperature/src/test/java/com/station/temperature/TemperatureUnitTest.java
@@ -1,11 +1,22 @@
 package com.station.temperature;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.util.List;
 import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.station.temperature.constant.ResultType;
 import com.station.temperature.entity.Temperature;
 import com.station.temperature.model.ReturnModel;
+import com.station.temperature.model.TimeIntervalModel;
 
 public class TemperatureUnitTest extends AbstractTest {
 
@@ -17,6 +28,20 @@ public class TemperatureUnitTest extends AbstractTest {
 		assertEquals(result, true);
 		
 	}
+	
+	@Test
+	public void testRestApi() throws Exception {
+		
+		MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+
+		String jsonStr = new ObjectMapper()
+				.writeValueAsString(new TimeIntervalModel("2021-12-28 15:00:00", "2021-12-28 15:30:00"));
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/temperature/average").content(jsonStr)
+				.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andDo(MockMvcResultHandlers.print());
+	}
+	
 	
 	@Test
 	public void testGetBetweenDatastampDao() {
